934ef60cd89af5e43a1fd2ac06b1d90149480206
ionut <ionut.grad@innio.com>
2024-10-29 23:57:51 +0200

    feat: SymbolsGrid adjustments

--- frontend/src/components/SymbolsView/SymbolsView.tsx
+++ frontend/src/components/SymbolsView/SymbolsView.tsx
@@ -1,25 +1,27 @@
+import "./symbolsView.css";
 import SymbolsGrid from '@/components/SymbolsGrid';
 import PriceChart from '@/components/PriceChart';
 import DesktopInfo from './src/DesktopInfo';
-import { useState } from 'react';
+import {useState} from 'react';
 
 const SymbolsView = () => {
   const [activeSymbol, setActiveSymbol] = useState<null | string>(null);
+
   const handleSymbolClick = (symbolId: string) => {
-    setActiveSymbol((s) => (s === symbolId ? null : symbolId));
+    setActiveSymbol((s) => s === symbolId ? null : symbolId);
   };
 
   return (
       <div className="symbolsView">
         <DesktopInfo/>
-        <div className="symbolsView__chart">
-          <h3>PRICE HISTORY</h3>
-        </div>
         <div className="symbolsView__content">
-          <PriceChart symbolId={activeSymbol}/>
-          <div className="symbolsView__cards">
-            <SymbolsGrid onSymbolClick={handleSymbolClick}/>
-          </div>
+            <div className="symbolsView__cards">
+                <SymbolsGrid activeSymbol={activeSymbol} onSymbolClick={handleSymbolClick}/>
+            </div>
+            <div className="symbolsView__chart">
+                <h3>PRICE HISTORY</h3>
+                <PriceChart symbolId={activeSymbol}/>
+            </div>
         </div>
       </div>
   );
